49f7ae0c1ddf218b7588fa2191be31a2da27355a
jaredhall18 <jaredhall18@gmail.com>
2021-12-03 02:50:56 -0700

    Errorhandling middleware and routing working.

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,11 @@
 // add middlewares here related to projects
+function errorHandling(err, req, res, next) {
+        res.status(err.status || 500).json({
+            message: `There was an error processing your request: ${err.message}`,
+            stack: err.stack,
+        })
+}
+
+module.exports = {
+    errorHandling,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,21 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const {
+    errorHandling,
+} = require('./projects-middleware')
+
+const Projects = require('./projects-model') 
+
+
+//`[GET] /api/projects`
+router.get('/', (req, res, next) => {
+    Projects.get(req.query)
+        .then(project => {
+            throw new Error("test")
+        })
+        .catch(next);
+    
+})
+router.use(errorHandling)
+module.exports = router
